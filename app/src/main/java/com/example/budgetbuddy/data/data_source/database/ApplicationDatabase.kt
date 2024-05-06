2c9790bfb7e62eaef60c3c751116718d54b23436
Renan Oliveira <renan.oliveira.rao@gmail.com>
2024-05-06 20:56:46 -0300

    Create class of database

---
+++ app/src/main/java/com/example/budgetbuddy/data/data_source/database/ApplicationDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.budgetbuddy.data.data_source.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.budgetbuddy.data.data_source.dao.BudgetDao
+import com.example.budgetbuddy.data.data_source.dao.ClientDao
+import com.example.budgetbuddy.data.data_source.dao.ProductDao
+import com.example.budgetbuddy.domain.model.Budget
+import com.example.budgetbuddy.domain.model.Client
+import com.example.budgetbuddy.domain.model.Product
+import com.example.budgetbuddy.domain.model.relations.BudgetProductCrossRef
+
+@Database(
+    entities = [Product::class, Client::class, Budget::class, BudgetProductCrossRef::class],
+    version = 1,
+    exportSchema = true
+)
+abstract class ApplicationDatabase: RoomDatabase() {
+    abstract val productDao: ProductDao
+    abstract val budgetDao: BudgetDao
+    abstract val clientDao: ClientDao
+
+    companion object{
+        const val DATABASE_NAME = "budget_db"
+    }
+}
