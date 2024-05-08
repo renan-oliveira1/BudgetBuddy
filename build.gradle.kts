87738c5592b3f5d3e76de62bf869138051069336
Renan Oliveira <renan.oliveira.rao@gmail.com>
2024-05-08 19:04:14 -0300

    Add dependencies of dagger hilt for dependency injection

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("androidx.room") version "$room_version" apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
